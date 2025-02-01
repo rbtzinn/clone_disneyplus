d4d639e37b5619664dd99a9d8f1ad16c952be1d3
Roberto <roberto.gabriel2004@hotmail.com>
2025-01-31 22:31:45 -0300

    configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,20 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 const replace = require('gulp-replace');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require ('gulp-uglify')
+
+function scripts(){
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+}
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(replace('/src/images/', '/dist/images/'))
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(cleanCSS()) 
+        .pipe(replace('/src/images/', '/dist/images/')) 
         .pipe(gulp.dest('./dist/css'));
 }
 
@@ -16,8 +25,9 @@ function images() {
         .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 exports.watch = function () {
     gulp.watch('./src/styles/**/*.scss', gulp.parallel(styles));
     gulp.watch('./src/images/**/*', gulp.parallel(images));
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
